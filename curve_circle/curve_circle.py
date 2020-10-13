bbce49471947f142b91820f259fb667ccbb06a82
Ethan B <eebmagic@gmail.com>
2020-10-13 11:58:56 -0400

    setup for recording

--- curve_circle/curve_circle.py
+++ curve_circle/curve_circle.py
@@ -17,12 +17,15 @@ t.hideturtle()
 t.speed("fastest")
 turtle.tracer(0, 0)
 
+import time
+start = input("Waiting for user to start...")
+time.sleep(4)
 
 # Set Constants
-length = 180 + 90
+length = 180 + 90 + 90
 startShift = 0
-count = 100
-shapeSize = 20
+count = 130
+shapeSize = 23
 turn = math.ceil((length) / count)
 
 # Main Loop
@@ -34,6 +37,7 @@ for i in range(startShift, startShift + length, turn):
     t.penup()
     t.home()
     t.pendown()
+    turtle.update()
 
 # Show drawing
 turtle.update()
